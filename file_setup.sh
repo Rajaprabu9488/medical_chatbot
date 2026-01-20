3e4d43792c25ed4055e72d6f7a1a41906fc7b490
Rajaprabu9488 <rajaprabhu484@gmail.com>
2026-01-20 12:19:03 +0530

    file structure upload

---
+++ file_setup.sh
@@ -0,0 +1,14 @@
+# making directory 
+mkdir books
+mkdir models
+mkdir src
+
+
+# create files
+touch models/rag_model.ipynb
+touch models/ocr_model.ipynb
+
+touch src/input_handler.py
+touch src/embedding_and_llm.py
+touch src/audio_to_text.py
+touch src/image_to_text.py
